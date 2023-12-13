1cb93e6f06ad650fa97c4370abb1f0fde72ffeb7
uladzislau.chernik <uladzislau.chernik@ukg.com>
2023-12-13 14:51:01 +0200

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+fun properties(key: String, defaultValue: String = ""): String =
+        project.findProperty(key)?.toString() ?: defaultValue
+
+plugins {
+    kotlin("jvm") version "1.9.21"
+    id("org.jetbrains.intellij") version "1.16.1"
+}
+
+group = properties("pluginGroup")
+version = properties("pluginVersion")
+
+val since = properties("sinceIdeaBuild")
+
+repositories {
+    mavenCentral()
+}
+
+intellij {
+    updateSinceUntilBuild.set(false)
+    type.set(properties("platformType"))
+    version.set(properties("platformVersion"))
+    plugins.set(
+            properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty)
+    )
+}
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    patchPluginXml {
+        sinceBuild.set(since)
+    }
+}
