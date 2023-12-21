7f7ee39bc3e83a3cd2eceb0efb3a14bee6dcf61a
uladzislau.chernik <uladzislau.chernik@ukg.com>
2023-12-21 13:28:22 +0200

    Change line marker location

--- src/main/kotlin/com/ukg/bozr/bozrplugin/runconfiguration/BozrRunLineMarkerContributor.kt
+++ src/main/kotlin/com/ukg/bozr/bozrplugin/runconfiguration/BozrRunLineMarkerContributor.kt
@@ -6,18 +6,21 @@ import com.intellij.icons.AllIcons
 import com.intellij.json.psi.JsonArray
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
 import com.ukg.bozr.bozrplugin.service.BozrFileService
 
 class BozrRunLineMarkerContributor : RunLineMarkerContributor() {
     private val bozrFileService = BozrFileService.getInstance()
 
     override fun getInfo(element: PsiElement): Info? =
-        element.let { it as? JsonArray }
-            ?.let { it.parent as? PsiFile }
+        element.takeIf { it.parent is JsonArray }
+            ?.takeIf { it.parent.parent  is PsiFile }
+            ?.takeIf { PsiManager.getInstance(it.project).areElementsEquivalent(it.parent.firstChild, it) }
+            ?.containingFile
             ?.takeIf { bozrFileService.isFileBozrTest(it) }
             ?.let {
                 Info(AllIcons.RunConfigurations.TestState.Run, ExecutorAction.getActions()) {
-                    "test"
+                    "Run Test"
                 }
             }
 }
